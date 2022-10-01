93b0b6b07901af9a33b46ad61b6fd9e310cdd3a5
Aman Gupta <aman.g@inmobi.com>
2022-10-02 00:06:59 +0530

    added test

---
+++ test.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from lambda_diffusers import StableDiffusionImageEmbedPipeline
+from PIL import Image
+import torch
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+pipe = StableDiffusionImageEmbedPipeline.from_pretrained("lambdalabs/sd-image-variations-diffusers")
+pipe = pipe.to(device)
+
+im = Image.open("../dataset/adidas_ad_1_by_mdr9inchnails_d3apxh9-fullview.jpeg")
+num_samples = 4
+image = pipe(num_samples*[im], guidance_scale=3.0)
+image = image["sample"]
+
+base_path = Path("outputs/im2im")
+base_path.mkdir(exist_ok=True, parents=True)
+for idx, im in enumerate(image):
+    im.save(base_path/f"{idx:06}.jpg")
